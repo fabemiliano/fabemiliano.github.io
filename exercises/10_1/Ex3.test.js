06e1c2a1f9e0b50a81e1dcf4440e27d003d91ed3
fabemiliano <fab.emiliano@gmail.com>
2020-04-16 18:41:56 -0300

    teste

--- exercises/10_1/Ex3.test.js
+++
@@ -1,9 +0,0 @@
-const Ex3 = require('./Ex3')
-
-
-// implemente seus testes aqui
-it ("testando exercicio 3",()=>{
-  expect(Ex3.mySum([1,2,3,4])).toBe(10)
-  expect(Ex3.mySum([1, -2, -3, 4])).toBe(0)
-}
-)
