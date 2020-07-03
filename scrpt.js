4240805589395ec172e116a8d7c2ca49a935949e
fabemiliano <fab.emiliano@gmail.com>
2020-07-03 16:50:00 -0300

    added progress bar

---
+++ scrpt.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.menu-container');
+const menu = document.querySelector('.menu-icon');
+container.classList.forEach(e => console.log(e))
+menu.addEventListener('click', () => {
+  console.log(container.classList)
+  container.classList.forEach(e => {
+
+    if (e === 'hidden') {
+      container.classList.remove('hidden')
+      container.classList.add('show-menu')
+    }
+    else if (e === 'show-menu') {
+      container.classList.remove('show-menu')
+      container.classList.add('not-show-menu')
+    }
+    else if (e === 'not-show-menu') {
+      container.classList.add('show-menu')
+      container.classList.remove('not-show-menu')
+    }
+  })
+});
+
+$(document).on('ready', function() {  
+  var winHeight = $(window).height(), 
+      docHeight = $(document).height(),
+      progressBar = $('progress'),
+      max, value;
+
+  /* Set the max scrollable area */
+  max = docHeight - winHeight;
+  progressBar.attr('max', max);
+
+  $(document).on('scroll', function(){
+     value = $(window).scrollTop();
+     progressBar.attr('value', value);
+  });
+});
